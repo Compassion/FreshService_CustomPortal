ac99dad3c9a796afb7b73bfa7587ab53e5a4787a
Mark Harvey <mharvey@compassion.com.au>
2017-09-18 14:17:37 +1000

    Create New_Ticket.js

---
+++ New_Ticket.js
@@ -0,0 +1,6 @@
+(function($){
+  
+  $('#s2id_helpdesk_ticket_custom_field_software_90393').on('select2:selecting', function(e) {
+    console.log('Selecting: ', e.params.args.data);
+  });
+})(jQuery);
